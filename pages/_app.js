e5d6c72c0b77923f0004194f806278d32261525c
Finn Woelm <public@finnwoelm.com>
2021-03-27 21:56:01 +0100

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,40 @@
+import App from "next/app";
+import Head from "next/head";
+import { useEffect } from "react";
+import { CssBaseline } from "@material-ui/core";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider } from "styled-components";
+import NextNProgress from "nextjs-progressbar";
+import { theme } from "../helpers/getTheme";
+
+function MyApp({ Component, pageProps }) {
+  // Remove the server-side injected CSS.
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Global Goals Directory - Admin Dashboard</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <NextNProgress />
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { Typography } from "@material-ui/core";
+
+const HomePage = () => (
+  <Typography variant="h1">Global Goals Directory Admin Dashboard</Typography>
+);
+
+export default HomePage;
