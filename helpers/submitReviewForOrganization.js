d798fa1178062e7c199816c1c638cc24a01ae3d4
Finn Woelm <public@finnwoelm.com>
2021-05-09 21:05:00 +0200

    On complete review, overwrite existing reviewed_extraction_data
    When user performs complete review, we clear the current reviewed
    extraction data before setting the new one. Essentially, this serves as
    a replace operation. By default, q.Update will merge the properties of
    two objects, so if we did not clear it first, we'd end up with old/stale
    data in the object.
    
    See https://forums.fauna.com/t/replace-by-path/169/2 for context.

--- helpers/submitReviewForOrganization.js
+++ helpers/submitReviewForOrganization.js
@@ -71,10 +71,30 @@ const submitReviewForOrganization = async ({
     );
   }
 
-  await client.query(
-    q.Update(q.Ref(q.Collection("organizations"), organization.faunaId), {
+  // Prepare Fauna actions
+  const faunaActions = [
+    q.Update(q.Var("ref"), {
       data: remapKeysToDatabase(updateData),
-    })
+    }),
+  ];
+
+  // Clear reviewed extraction data, then set new reviewed extraction data.
+  // Essentially, this serves as a replace operation. By default, q.Update will
+  // merge the properties of two objects, so if we did not clear it first, we'd
+  // end up with old/stale data in the object.
+  // See https://forums.fauna.com/t/replace-by-path/169/2 for context.
+  if (isCompleteReview)
+    faunaActions.unshift(
+      q.Update(q.Var("ref"), {
+        data: remapKeysToDatabase({ reviewedExtractionData: null }),
+      })
+    );
+
+  await client.query(
+    q.Let(
+      { ref: q.Ref(q.Collection("organizations"), organization.faunaId) },
+      q.Do(...faunaActions)
+    )
   );
 };
 
